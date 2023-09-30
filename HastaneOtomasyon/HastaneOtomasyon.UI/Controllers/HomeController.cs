d2ab35c4b1e79a6698f217b81eb4f70525c4c305
MuratAkgol <akgolmurat4@gmail.com>
2023-09-30 15:00:03 +0300

    UI Çalışması başlatıldı

---
+++ HastaneOtomasyon/HastaneOtomasyon.UI/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using DataLayer;
+using EntityLayer;
+using HastaneOtomasyon.UI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace HastaneOtomasyon.UI.Controllers
+{
+    public class HomeController : Controller
+    {
+        Context db = new Context();
+        Anasayfa ana = new Anasayfa();
+
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            ana.Doktorlar = db.tbl_Doktorlar.ToList();
+            ana.Pol = db.tbl_Polikinlikler.ToList();
+            return View(ana);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ HastaneOtomasyon/HastaneOtomasyon.UI/Controllers/RandevuController.cs
@@ -0,0 +1,28 @@
+﻿using DataLayer;
+using EntityLayer;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HastaneOtomasyon.UI.Controllers
+{
+    public class RandevuController : Controller
+    {
+        Context db = new Context();
+        Anasayfa ana = new Anasayfa();
+        public IActionResult Index()
+        {
+            ana.Pol = db.tbl_Polikinlikler.ToList();
+            ana.Doktorlar = db.tbl_Doktorlar.ToList();
+            
+            
+            return View(ana);
+        }
+        public IActionResult SecilenPolDr(Polikinlik pol)
+        {
+            int id =pol.PolikinlikId;
+            ana.Pol = db.tbl_Polikinlikler.ToList();
+            ana.Doktorlar = db.tbl_Doktorlar.Where(x=>x.PolikinlikId == id).ToList();
+            return View(ana);
+        }
+
+    }
+}
